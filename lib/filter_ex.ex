2668f53a2e5c3e56b6b1cf55110fcdb2e2554fcf
Jaremy Creechley <creechley@gmail.com>
2024-11-18 13:43:16 -0700

    initial commit

---
+++ lib/filter_ex.ex
@@ -0,0 +1,18 @@
+defmodule FilterEx do
+  @moduledoc """
+  Documentation for `FilterEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FilterEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/filter_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule FilterExTest do
+  use ExUnit.Case
+  doctest FilterEx
+
+  test "greets the world" do
+    assert FilterEx.hello() == :world
+  end
+end

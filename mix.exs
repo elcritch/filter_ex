ccebc902e2c31404d0899a4fe5aeb41297c7c6fc
Jaremy Creechley <creechley@gmail.com>
2024-11-19 00:38:27 -0700

    add ExpAverage tests

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FilterEx.MixProject do
   def project do
     [
       app: :filter_ex,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

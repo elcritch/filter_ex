024bb54970e45b6c57c2609f68740466ceafa543
Jaremy Creechley <creechley@gmail.com>
2024-11-19 00:24:28 -0700

    more tests

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FilterEx.MixProject do
   def project do
     [
       app: :filter_ex,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
